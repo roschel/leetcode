7ce83b75ba8ef3a3e3e2b43de6d6f124c878a38e
roschel <joaomroschel@gmail.com>
2024-08-01 15:46:02 -0300

    longest_palindrome_substring

--- longest_palindromic_substring.py
+++ longest_palindromic_substring.py
@@ -26,13 +26,24 @@ class Solution:
         if n <= 1:
             return s
 
-        max_string = ""
-        for i in range(n - 1):
-            for j in range(n):
-                if s[i:j + 1] == s[i:j + 1][::-1]:
-                    max_string = s[i:j + 1] if len(s[i:j + 1]) > len(max_string) else max_string
+        def expand_from_center(left, right):
+            while left >= 0 and right < n and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
 
-        return max_string
+        max_str = s[0]
+
+        for i in range(n - 1):
+            odd = expand_from_center(i, i)
+            even = expand_from_center(i, i + 1)
+
+            if len(odd) > len(max_str):
+                max_str = odd
+            if len(even) > len(max_str):
+                max_str = even
+
+        return max_str
 
 
 if __name__ == '__main__':
